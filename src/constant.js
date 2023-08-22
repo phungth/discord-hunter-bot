a90f41014ddb8a49f2c0a921f940cf93523d3fd2
phungth <phungth@vng.com.vn>
2023-08-22 09:01:41 +0700

    feat: add avt

--- src/constant.js
+++ src/constant.js
@@ -1,8 +1,32 @@
 export const members = [
-	{ name: 'XHunter', value: 'phungth' },
-	{ name: 'Hiepsikhokhao', value: 'locth2' },
-	{ name: 'tranvanhuyhoang', value: 'hoangtvh' },
-	{ name: 'nguyenhaia8', value: 'haint11' },
-	{ name: 'Melchizedek', value: 'huyvh' },
-	{ name: 'Anonymous', value: 'linhnn6' },
+	{
+		name: 'XHunter',
+		value: 'phungth',
+		avt: 'https://avatars.cloudflare.steamstatic.com/087e8e89676a805f7d0e530e6b4aafc5016662e7_full.jpg',
+	},
+	{
+		name: 'Hiepsikhokhao',
+		value: 'locth2',
+		avt: 'https://avatars.cloudflare.steamstatic.com/d0c6521c84eee5016f6a37733e3ec19120134b29_full.jpg',
+	},
+	{
+		name: 'tranvanhuyhoang',
+		value: 'hoangtvh',
+		avt: 'https://avatars.cloudflare.steamstatic.com/a8a7a620c8e34934807bbc76ed92892118c56c71_full.jpg',
+	},
+	{
+		name: 'nguyenhaia8',
+		value: 'haint11',
+		avt: 'https://avatars.cloudflare.steamstatic.com/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_full.jpg',
+	},
+	{
+		name: 'Melchizedek',
+		value: 'huyvh',
+		avt: 'https://avatars.cloudflare.steamstatic.com/02f06f076609d308a76de8bad01662e2ead03320_full.jpg',
+	},
+	{
+		name: 'Anonymous',
+		value: 'linhnn6',
+		avt: 'https://cdn.discordapp.com/avatars/782831531999100988/19cbbaf52ac758390d3c6b31e9065e8a.webp?size=512',
+	},
 ];
